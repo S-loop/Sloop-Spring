f30ead410b81feece36875e94140378b6b9feca5
seokeunpark <pse620@gmail.com>
2024-01-03 08:17:53 +0900

    docs: 테이블 변수 추가

--- src/main/java/kr/co/sloop/notice/domain/NoticeDTO.java
+++ src/main/java/kr/co/sloop/notice/domain/NoticeDTO.java
@@ -4,9 +4,11 @@ import lombok.Data;
 
 @Data
 public class NoticeDTO {
-	
-	/*** 테이블 변수***/
+
+    /*** 테이블 변수***/
     private Integer postIdx;					/*게시판 키*/
+    private Integer prevPostIdx;				/*이전글 게시판 키*/
+    private Integer nextPostIdx;				/*다음글 게시판 키*/
     private String 	postNoticeTitle;			/*게시판 제목*/
     private String 	postNoticeContents;			/*게시판 내용*/
     private Integer memberIdx;					/*회원 키*/
@@ -15,18 +17,18 @@ public class NoticeDTO {
     private Integer postNoticeHits;				/*조회수*/
     private Integer categoryPostIdx;			/*카테고리 키*/
     private String 	postNoticePinned;			/*상단 고정여부*/
-    
+
     /*** 페이징 관련 ***/
     /** 현재페이지 */
-	private int page = 1;
-	/** 페이지당 출력 개수 */
-	private int row = 10;
-	//mySql용 변수
-	private int startIndex = 0;
-	/*검색조건*/
-	private String searchType;
-	/*검색 값*/
-	private String searchTxt;
+    private int page = 1;
+    /** 페이지당 출력 개수 */
+    private int row = 10;
+    //mySql용 변수
+    private int startIndex = 0;
+    /*검색조건*/
+    private String searchType;
+    /*검색 값*/
+    private String searchTxt;
 	
 	
 	/*       
