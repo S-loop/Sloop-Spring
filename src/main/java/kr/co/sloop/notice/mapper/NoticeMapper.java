3b9eeb835eeb0be7ced20253a0965f43dc9bb34a
seokeunpark <pse620@gmail.com>
2024-01-03 08:18:39 +0900

    docs: 조회수 증가 추가

--- src/main/java/kr/co/sloop/notice/mapper/NoticeMapper.java
+++ src/main/java/kr/co/sloop/notice/mapper/NoticeMapper.java
@@ -8,23 +8,26 @@ import kr.co.sloop.notice.domain.NoticeDTO;
 
 @Mapper
 public interface NoticeMapper {
-	
+
 	//페이징 공지사항 목록조회
 	public List<NoticeDTO> selectListNoticePaging(NoticeDTO noticeDto) throws Exception;
-	
+
 	//공지사항 갯수
 	public int selectNoticeCnt(NoticeDTO noticeDto) throws Exception;
-	
+
 	//공지사항 상세조회
 	public NoticeDTO selectDetailNotice(NoticeDTO noticeDto) throws Exception;
 
 	//공지사항 저장
 	public int insertNotice(NoticeDTO noticeDto) throws Exception;
-	
-    //공지사항 삭제
+
+	//공지사항 삭제
 	public int deleteNotice(NoticeDTO noticeDto) throws Exception;
-    
-    //공지사항 수정
+
+	//공지사항 수정
 	public int updateNotice(NoticeDTO noticeDto) throws Exception;
+
+	//공지사항 조회수 증가
+	public void updatePostNoticeHits(Integer postIdx) throws Exception;
 
 }
